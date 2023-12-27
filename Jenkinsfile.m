eef45158e493ab09bdca6fc5667e60c847b808a5
Anil Aithal <152262171+aithalias@users.noreply.github.com>
2023-12-27 09:05:50 +0530

    Update Jenkinsfile.m(error fixed in dev branch)

--- Jenkinsfile.m
+++ Jenkinsfile.m
@@ -1,13 +1,9 @@
 pipeline{
     agent none
-	triggers {cron '''H H * * *'''
-			  pollSCM '*/5 * * * *'
-			  }
-	environment{
+    	environment{
 	 STAGE_ONE = 'SCM_checkout'
 	 STAGE_TWO = 'code_analysis'
 	 STAGE_THREE = 'maven_build'
-	 SSH_CREDS = credentials('ssh')
 	}
 	parameters {
         string(name: 'PERSON', defaultValue: 'Developer1', description: 'Who is executing the task?')
@@ -20,53 +16,54 @@ pipeline{
             stage('git checkout'){
                 agent{label 'agent3'}
                 steps{
-					echo "Executor : ${params.PERSON} in ${params.ENV}"
-                    git 'https://github.com/aithalias/java_repo1.git'
-					echo "Executing ${env.STAGE_ONE}"
+			echo "Executor : ${params.PERSON} in ${params.ENV}"
+                    	git 'https://github.com/aithalias/java_repo1.git'
+			echo "Executing ${env.STAGE_ONE}"
                 }
             }
             stage('Sonar analysis'){
                 agent{label 'agent3'}
                 steps{
-                    withSonarQubeEnv('sonar'){
-                    sh "mvn clean verify sonar:sonar -Dsonar.projectKey=assignment5"
-					echo "executed ${env.STAGE_TWO}"
-                    }
-                }
+                    	withSonarQubeEnv('sonar'){
+                   	sh "mvn clean verify sonar:sonar -Dsonar.projectKey=assignment5a"
+		    	echo "executed ${env.STAGE_TWO}"
+                   	 }
+               	}
             }
             stage('Build stage'){
                 agent{label 'agent2'}
-				when {
-                expression {
-					return currentBuild.resultIsBetterOrEqualTo('SUCCESS') &&
-                    (env.BRANCH_NAME == 'master' || env.BRANCH_NAME == 'dev')
-                }
-            }
+			when {
+                	expression {
+				return currentBuild.resultIsBetterOrEqualTo('SUCCESS') &&
+                    		(env.BRANCH_NAME == 'master' || env.BRANCH_NAME == 'dev')
+                		}
+            		}
                 steps{
-                    git 'https://github.com/aithalias/java_repo1.git'
-                    sh "mvn clean install"
-					echo "executed ${env.STAGE_THREE}"
+                   	git 'https://github.com/aithalias/java_repo1.git'
+                    	sh "mvn clean install"
+			echo "executed ${env.STAGE_THREE}"
                 }
             }
             stage('Deploy to Tomcat'){
                 agent{label 'agent2'}
-				when {
+			environment{
+			SSH_CREDS = credentials('ssh')
+			}
+		when {
                 expression {
                     return currentBuild.resultIsBetterOrEqualTo('SUCCESS')
                 }
             }
-                steps{
-					sh 'echo "SSH private key is located at $SSH_CREDS"'
-                    sh 'echo "SSH user is $SSH_CREDS_USR"'
-					sh 'mkdir -p versions'
-					sh 'sudo cp target/*.war versions/works-with-heroku-$BUILD_ID.war'
-                    sh "sudo cp versions/works-with-heroku-$BUILD_ID.war' /opt/tomcat/webapps/"
-					echo "Deployed in ${params.ENV} environment"
+                steps{				
+			sh 'echo "SSH private key is located at $SSH_CREDS"'
+                    	sh 'echo "SSH user is $SSH_CREDS_USR"'
+			sh 'sudo cp target/*.war /opt/tomcat/webapps/'
+			echo "Deployed in ${params.ENV} environment"
                 }
             }
         }
-		post { 
-        always { 
+	post { 
+         always { 
             script{
 		timeout(time: 10, unit: 'MINUTES') {
               def approvalMailContent = """
@@ -75,14 +72,14 @@ pipeline{
               Go to build URL and check the console output.
               URL de build: ${env.BUILD_URL}
               """
-			  mail(
+		mail(
              to: 'aithal96.anil@gmail.com',
              subject: "${currentBuild.result} CI: Project name -> ${env.JOB_NAME}", 
              body: approvalMailContent,
              mimeType: 'text/plain'
               )
-		    }
-	      }
-        }
+	    }
+	 }
+      }
     }
 }
